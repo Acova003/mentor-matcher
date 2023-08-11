e3df9b448ab092f071d3f0d13834a76b5df391a7
Amee <ameecova@yahoo.com>
2023-08-11 13:15:07 +0200

    Match working with hard coded json

--- routes/match.js
+++ routes/match.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var router = express.Router();
 const db = require("../model/helper");
-const openai = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_API_KEY,
+});
 
-// .env imports
-//
-const apiKey = process.env.VITE_OPEN_AI_API_KEY;
+const openai = new OpenAIApi(configuration);
 
 // Send all mentor and mentee questionnaire responses to chatgpt to generate matches
 router.post("/", async function (req, res, next) {
@@ -29,6 +30,7 @@ const getQuestionnaireResponses = async (tableName) => {
     const responses = await db(
       `SELECT questionnaire_responses FROM ${tableName};`
     );
+    console.log(responses.data.map((entry) => entry.questionnaire_responses));
     return responses.data.map((entry) => entry.questionnaire_responses);
   } catch (error) {
     console.error(error);
@@ -45,10 +47,91 @@ const generatePrompt = (mentorResponses, menteeResponses) => {
 
 const generateMatches = async (mentorResponses, menteeResponses) => {
   console.log();
+  testPrompt = {
+    mentors: [
+      {
+        mentor_id: 1,
+        questions: [
+          "Tell us about your programming experience.",
+          "What technologies are you proficient in?",
+          "How do you approach code optimization?",
+          "What's your mentoring style?",
+          // Add more questions
+        ],
+        answers: [
+          "I have 10 years of experience in Java development.",
+          "I'm proficient in Python, Django, and SQL.",
+          "I focus on algorithm efficiency and minimizing database queries.",
+          "I believe in hands-on, project-based learning.",
+          // Add more answers
+        ],
+      },
+      {
+        mentor_id: 2,
+        questions: [
+          "Tell us about your programming experience.",
+          "What technologies are you proficient in?",
+          "How do you approach code optimization?",
+          "What's your mentoring style?",
+          // Add more questions
+        ],
+        answers: [
+          "I have 5 years of experience in web development.",
+          "I'm proficient in JavaScript, React, and Node.js.",
+          "I prioritize clean code and reducing redundant operations.",
+          "I prefer guiding mentees through real-world projects.",
+          // Add more answers
+        ],
+      },
+      // Add more mentors
+    ],
+    mentees: [
+      {
+        mentee_id: 1,
+        questions: [
+          "What are your goals in learning programming?",
+          "Do you have any experience with version control?",
+          "What projects have you worked on before?",
+          "How do you handle debugging complex issues?",
+          // Add more questions
+        ],
+        answers: [
+          "I want to become a full-stack developer.",
+          "Yes, I'm familiar with Git and GitHub.",
+          "I've built a basic e-commerce website using React.",
+          "I break down the problem and use debugging tools step by step.",
+          // Add more answers
+        ],
+      },
+      {
+        mentee_id: 2,
+        questions: [
+          "What are your goals in learning programming?",
+          "Do you have any experience with version control?",
+          "What projects have you worked on before?",
+          "How do you handle debugging complex issues?",
+          // Add more questions
+        ],
+        answers: [
+          "I aim to specialize in mobile app development.",
+          "I've used Git for personal projects.",
+          "I've created mobile apps using Flutter.",
+          "I consult documentation and seek help from online communities.",
+          // Add more answers
+        ],
+      },
+      // Add more mentees
+    ],
+  };
+
   try {
-    const prompt = `return mentor and mentee matches for ${generatePrompt(
-      mentorResponses,
-      menteeResponses
+    // const prompt = `return mentor and mentee matches for ${generatePrompt(
+    //   mentorResponses,
+    //   menteeResponses
+    // )}. I want one mentor per mentee and one mentee per mentor at a software engineering bootcamp. I want to match based on the following criteria: I want a compatibility score with a value from 1-100 and a description of why their compatible with a maximum of 500 characters.`;
+
+    const prompt = `return mentor and mentee matches for ${JSON.stringify(
+      testPrompt
     )}. I want one mentor per mentee and one mentee per mentor at a software engineering bootcamp. I want to match based on the following criteria: I want a compatibility score with a value from 1-100 and a description of why their compatible with a maximum of 500 characters.`;
 
     const schema = {
@@ -63,18 +146,17 @@ const generateMatches = async (mentorResponses, menteeResponses) => {
       },
     };
 
-    const response = await openai.Completion.create({
-      model: "gpt-3.5-turbo-0613",
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
       messages: [
         { role: "system", content: "You are a helpful mentor/mentee matcher." },
         { role: "user", content: prompt },
       ],
-      functions: [{ name: "set_match_results", parameters: schema }],
-      function_call: { name: "set_match_results" },
+      //   functions: [{ name: "set_match_results", parameters: schema }],
+      //   function_call: { name: "set_match_results" },
     });
 
-    const generatedText =
-      response.data.choices[0].message.function_call.arguments;
+    const generatedText = response.data.choices[0].message.content;
     const matchResults = JSON.parse(generatedText);
 
     return matchResults;
