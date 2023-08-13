8b02adf0c15b1d2d4f2dc64cfbdb60de9b06abad
Amee <ameecova@yahoo.com>
2023-08-13 22:07:50 +0200

    Chatgpt call and response format working

--- routes/match.js
+++ routes/match.js
@@ -19,6 +19,18 @@ router.post("/", async function (req, res, next) {
     );
 
     res.json({ matchResults });
+
+    // insert match results into database mentor_mentee table
+
+    // for (let match of matchResults.matches) {
+    //   const { mentor_id, mentee_id, compatibility_score, description } = match;
+
+    //   const query =
+    //     "INSERT INTO mentor_mentee (mentor_id, mentee_id, compatibility_score, compatibility_description) " +
+    //     `VALUES ("${mentor_id}", "${mentee_id}", "${compatibility_score}", "${description}")`;
+
+    //   await db(query);
+    // }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred" });
@@ -28,123 +40,99 @@ router.post("/", async function (req, res, next) {
 const getQuestionnaireResponses = async (tableName) => {
   try {
     const responses = await db(
-      `SELECT questionnaire_responses FROM ${tableName};`
+      `SELECT id, q1, q2, q3, q4, q5, q6, q7 FROM ${tableName};`
     );
-    console.log(responses.data.map((entry) => entry.questionnaire_responses));
-    return responses.data.map((entry) => entry.questionnaire_responses);
+
+    return responses.data;
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
 
-const generatePrompt = (mentorResponses, menteeResponses) => {
-  const prompt = `Mentor: ${mentorResponses.join(
-    "\nMentor: "
-  )}\n\nMentee: ${menteeResponses.join("\nMentee: ")}`;
-  return prompt;
-};
-
 const generateMatches = async (mentorResponses, menteeResponses) => {
-  console.log();
-  testPrompt = {
-    mentors: [
-      {
-        mentor_id: 1,
-        questions: [
-          "Tell us about your programming experience.",
-          "What technologies are you proficient in?",
-          "How do you approach code optimization?",
-          "What's your mentoring style?",
-          // Add more questions
-        ],
-        answers: [
-          "I have 10 years of experience in Java development.",
-          "I'm proficient in Python, Django, and SQL.",
-          "I focus on algorithm efficiency and minimizing database queries.",
-          "I believe in hands-on, project-based learning.",
-          // Add more answers
-        ],
+  const formatDataSet = {
+    // Create a questions table in the database for better scalability
+    questions: {
+      mentors: {
+        q1: "Tell us about your career experience in tech and what technologies are you proficient in?",
+        q2: "What are you most passionate about in your career?",
+        q3: "What do you believe are the most important qualities of a successful mentor-mentee relationship?",
+        q4: "What are you hoping to get out of being a mentor?",
+        q5: "What would an ideal mentee profile look like for you?",
+        q6: "What was your career before your time at CodeOp?",
+        q7: "Are there any parts of your identity that might make you somebody’s role model? (eg: gender, ethnicity, sexual orientation, cultural background)?",
       },
-      {
-        mentor_id: 2,
-        questions: [
-          "Tell us about your programming experience.",
-          "What technologies are you proficient in?",
-          "How do you approach code optimization?",
-          "What's your mentoring style?",
-          // Add more questions
-        ],
-        answers: [
-          "I have 5 years of experience in web development.",
-          "I'm proficient in JavaScript, React, and Node.js.",
-          "I prioritize clean code and reducing redundant operations.",
-          "I prefer guiding mentees through real-world projects.",
-          // Add more answers
-        ],
+
+      mentees: {
+        q1: "What are your career goals? Is there a specific role in tech that you are interested in?",
+        q2: "What would be your ideal mentor profile?",
+        q3: "What do you believe are the most important qualities of a successful mentor-mentee relationship?",
+        q4: "Are there any parts of your identity that you would like to see reflected in a mentor? (eg: gender, ethnicity, sexual orientation, cultural background)",
+        q5: "What skills do you want to develop?",
+        q6: "What was your career before joining CodeOp?",
+        q7: "What do you hope to achieve through mentorship?",
       },
-      // Add more mentors
-    ],
-    mentees: [
-      {
-        mentee_id: 1,
-        questions: [
-          "What are your goals in learning programming?",
-          "Do you have any experience with version control?",
-          "What projects have you worked on before?",
-          "How do you handle debugging complex issues?",
-          // Add more questions
-        ],
-        answers: [
-          "I want to become a full-stack developer.",
-          "Yes, I'm familiar with Git and GitHub.",
-          "I've built a basic e-commerce website using React.",
-          "I break down the problem and use debugging tools step by step.",
-          // Add more answers
-        ],
-      },
-      {
-        mentee_id: 2,
-        questions: [
-          "What are your goals in learning programming?",
-          "Do you have any experience with version control?",
-          "What projects have you worked on before?",
-          "How do you handle debugging complex issues?",
-          // Add more questions
-        ],
-        answers: [
-          "I aim to specialize in mobile app development.",
-          "I've used Git for personal projects.",
-          "I've created mobile apps using Flutter.",
-          "I consult documentation and seek help from online communities.",
-          // Add more answers
-        ],
-      },
-      // Add more mentees
-    ],
+    },
+    mentorResponses: mentorResponses,
+    menteeResponses: menteeResponses,
   };
 
   try {
-    // const prompt = `return mentor and mentee matches for ${generatePrompt(
-    //   mentorResponses,
-    //   menteeResponses
-    // )}. I want one mentor per mentee and one mentee per mentor at a software engineering bootcamp. I want to match based on the following criteria: I want a compatibility score with a value from 1-100 and a description of why their compatible with a maximum of 500 characters.`;
-
-    const prompt = `return mentor and mentee matches for ${JSON.stringify(
-      testPrompt
-    )}. I want one mentor per mentee and one mentee per mentor at a software engineering bootcamp. I want to match based on the following criteria: I want a compatibility score with a value from 1-100 and a description of why their compatible with a maximum of 500 characters.`;
-
-    const schema = {
-      // mentee (student) id as key
-      mentee_id: {
-        // mentor id as key
-        mentor_id: "number",
-        // compatibility score as key
-        compatibility_score: "number",
-        // description of why they're compatible as key
-        description: "string",
+    const schemaExample = {
+      matches: [
+        {
+          mentor_id: 2,
+          mentee_id: 1,
+          compatibility_score: 85,
+          description: "Example description about compatibility.",
+        },
+      ],
+      unmatched: {
+        mentors: [1, 3],
       },
     };
+
+    // ***** WARNING: Do not change the prompt. It is formatted to work with the chatgpt model... unless you want to change the model. *****
+    const prompt = `
+Based on the provided data for ${menteeResponses.length} mentees and ${
+      mentorResponses.length
+    } mentors, I need you to:
+
+1. Analyze the responses and match each mentee to one mentor for a software engineering bootcamp.
+2. Determine their compatibility using the given questions and answers.
+3. Generate a compatibility score ranging from 1 to 100.
+4. Craft a description, that's between 400-500 characters, explaining their compatibility.
+5. Make sure every mentee is paired with only one mentor.
+6. Crucially, include any unmatched mentor's ID in the 'unmatched mentors' section.
+7. Provide the results in the following JSON structure: ${JSON.stringify(
+      schemaExample
+    )}
+8. Do not include anything outside of the curly braces of the JSON object.
+9. Ensure the returned results do not contain the original question answers.
+
+It's essential to follow these guidelines strictly.
+`;
+
+    // const schema = {
+    //   matches: [
+    //     {
+    //       mentor_id: "mentor_id",
+    //       mentee_id: "mentee_id",
+    //       compatibility_score: "compatibility_score an integer from 1-100",
+    //       description: "description a string with a maximum of 500 characters",
+    //     },
+    //   ],
+    //   unmatched: {
+    //     mentors: [
+    //       "An simple array of just ids of mentors that were not matched with a mentee. Neither question answers or a description of why they were not matched are not required.",
+    //     ],
+    //   },
+    // };
+
+    // const prompt = `return mentor and mentee matches for ${JSON.stringify(
+    //   formatDataSet
+    // )}. I want one mentor per mentee at a software engineering bootcamp. I want to match based on a compatibility score with a value from 1-100 and I want you to write a description of why they are compatible with a maximum of 500 characters. Don't include unmatched mentors in matches. I want you to give me the results in JSON with this format ${schema}. Since mentees should only be matched once,  the IDs of all unmatched mentors should be added to the unmatched mentors array. The question answers do not need to be included in the results.`;
 
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
@@ -152,18 +140,32 @@ const generateMatches = async (mentorResponses, menteeResponses) => {
         { role: "system", content: "You are a helpful mentor/mentee matcher." },
         { role: "user", content: prompt },
       ],
-      //   functions: [{ name: "set_match_results", parameters: schema }],
-      //   function_call: { name: "set_match_results" },
     });
 
-    const generatedText = response.data.choices[0].message.content;
-    const matchResults = JSON.parse(generatedText);
+    let parsedData;
+    console.log(
+      `This is all of my data ${response.data.choices[0].message.content}`
+    );
 
-    return matchResults;
+    try {
+      parsedData = JSON.parse(response.data.choices[0].message.content);
+    } catch (error) {
+      console.error("Error parsing JSON: ", error);
+      return;
+    }
+
+    return parsedData;
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
+
+// TO DO
+// BUG: escape ' in questionnaire responses
+// BUG: escape " in questionnaire responses
+// BUG: escape \ in questionnaire responses
+// BUG: escape \n in questionnaire responses
+// BUG: escape \r in questionnaire responses
 
 module.exports = router;
