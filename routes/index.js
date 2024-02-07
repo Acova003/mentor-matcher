56937548084378e996bd70d94ed0852f59ec5d30
Amee <ameecova@yahoo.com>
2024-02-07 15:28:43 +0100

    Use cors as middleware

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const app = express();
+
+// Use CORS middleware
+app.use(cors());
 
 // Serve static files from the build directory
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-// Serve the index.html file as the homepage
+// Define routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
+
+// Define other routes as needed
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
