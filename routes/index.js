aff92f167e226cbeaba4188e25be324e1b29ce60
Amee <ameecova@yahoo.com>
2024-02-07 16:50:56 +0100

    Use router

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const app = express();
+var router = express.Router();
 
 // Use CORS middleware
 app.use(cors());
@@ -10,11 +11,11 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "client", "build")));
 
 // Define routes
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-// Define other routes as needed
+app.use("/", router);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
